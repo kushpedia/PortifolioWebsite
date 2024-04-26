2aab9f0019ecb72d1fe4fbdb398354af56f6c35b
Henry Wanjiru <haronkuria2015@gmail.com>
2024-04-26 07:45:47 +0300

    removed duplicate about details

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -23,38 +23,12 @@ const About = () => {
 								A strong backend foundation in Python, Php and SQL and Mongodb databases. </p>
 							<p className='text-xl'>Passionate about problem-solving and creating efficient, user-friendly software solutions.</p>
 					</div>
+					<div>
 					<AboutMeTabs/>
-
-				
-			</div>					
+					</div>				
+				</div>					
 			</div>
-			</div>
-		
-
-
-
-
-
-
-		<div className='about_me_container'>
-			
-			<div className="surface-0 text-700 text-center">
-			<div className="text-blue-600 font-bold mb-1">
-				<p className='text-6xl'> &nbsp; <span className='about_ab'>Ab</span>out Me</p><i className="pi pi-slack" ></i>
-			</div>
-			<div className="text-900 font-bold text-5xl mb-1">Experienced Full-Stack Developer</div>
-			<img src={Experience} alt="Experience Iamge" className='experience_image' />
-			<div className="text-400 text-2xl mb-2 about_me">			
-				<Card title='Experience' className='shadow-5 font-semibold'>
-					<p className='text-xl'>Experienced full-stack developer skilled in building robust web applications.</p>
-					<p className='text-xl'>Proficient in frontend technologies like JavaScript, React, and CSS, 
-						A strong backend foundation in Python, Php and SQL and Mongodb databases. </p>
-					<p className='text-xl'>Passionate about problem-solving and creating efficient, user-friendly software solutions.</p>
-					<Button label="Hire Me" icon="pi pi-discord" className="font-bold px-2 py-2 white-space-nowrap hire_me_button" rounded raised />
-				</Card>
-			</div>			
-		</div>			
-	</div>
+		</div>
 			</>
 		
 	)
