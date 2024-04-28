fe63da4842f5abbe06e1ad588c566ebebaada36d
Henry Wanjiru <haronkuria2015@gmail.com>
2024-04-28 07:26:53 +0300

    added about me icons

--- src/Components/AboutMeTabs.jsx
+++ src/Components/AboutMeTabs.jsx
@@ -44,22 +44,22 @@ const AboutMeTabs = () => {
                     <p className="m-0 items-design">
                         <ul>
 							<li>
-								<h5>Maseno University</h5>
+								<h5><li className='pi pi-prime'></li> Maseno University</h5>
 								<p>Bachelor's Degree Information Technology</p>
 							</li>
 							<li>
-								<h5>Udemy</h5>
+							<h5> <li className='pi pi-objects-column'></li> Udemy</h5>
 								<p>100 days of code python bootcamp</p>
 								<p>The Ultimate React JS Course 2024</p>
 								<p>Python django</p>
 								<p>Introduction to Service Management with ITIL 4</p>
 							</li>
 							<li>
-								<h5>Naaro High School</h5>
+							<h5><li className='pi pi-pen-to-square'></li> Naaro High School</h5>
 								<p>Kenya Certificate of Secondary Education</p>
 							</li>
 							<li>
-								<h5>Kimotho Primary School</h5>
+							<h5><li className='pi pi-map'></li> Kimotho Primary School</h5>
 								<p>Kenya Certificate of Primary Education</p>
 							</li>
 							
